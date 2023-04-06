96d977af422d3bc4c419a59bef5f3ea3382d04fd
Felipe Cavalcante Lacerda <92037342+Felpasw@users.noreply.github.com>
2023-04-06 13:31:00 -0300

    Update Connect.ts

--- src/Database/Connect.ts
+++ src/Database/Connect.ts
@@ -2,4 +2,4 @@ import mongoose from 'mongoose';
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://felipasso:felipasso@cluster0.fovabzu.mongodb.net/LeadSoft?retryWrites=true&w=majority").then(() => console.log("Conected to mongoDB!"));
+mongoose.connect(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.PASSWORD}@cluster0.fovabzu.mongodb.net/LeadSoft?retryWrites=true&w=majority`).then(() => console.log("Conected to mongoDB!"));
